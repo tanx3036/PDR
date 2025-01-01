deb278d107b4410137205577d5efa6af0b2dc189
timothylin <timothylinziqi@gmail.com>
2025-01-01 14:58:12 -0800

    t3 app initialization

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["pdr_ai_v2_*"],
+} satisfies Config;
