6f0fc32461a55abedd5d8362c0ac5462630e3989
timothylin <timothylinziqi@gmail.com>
2025-01-05 16:15:01 -0800

    backend document deletion

---
+++ src/app/api/deleteDocument/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+import { db } from "../../../server/db/index";
+import { company, document, users } from "../../../server/db/schema";
+import { eq } from "drizzle-orm";
+import * as console from "console";
+
+
+export async function DELETE(request: Request) {
+    try {
+        const body = await request.json();
+        const { docId } = body;
+
+        await db.delete(document).where(eq(id, docId));
+
+        // Return a success response
+        return NextResponse.json({ success: true }, { status: 200 });
+    } catch (error) {
+        console.error('Error deleting document:', error);
+        return NextResponse.json({ error: 'Error deleting document' }, { status: 500 });
+    }
+}
